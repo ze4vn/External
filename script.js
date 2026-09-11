d722f83b89037246a71807f6876fa1056542d41d
Zev <nixonuxi44@gmail.com>
2026-09-11 14:18:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const pageTitles = {
 const pageTitle = document.getElementById("PageTitle");
 const searchbar = document.getElementById("Searchbar");
 const navButtons = document.querySelectorAll(".NavButton");
-const mobileNav = document.getElementById("MobileNav");
 const projectGrid = document.getElementById("ProjectGrid");
 
 const addButton = document.getElementById("AddButton");
@@ -251,7 +250,6 @@ function changePage(page) {
     navButtons.forEach(button => {
         button.classList.toggle("active", button.dataset.page === page);
     });
-    if (mobileNav) mobileNav.value = page;
     displayProjects();
 }
 
@@ -260,12 +258,6 @@ navButtons.forEach(button => {
         changePage(button.dataset.page);
     });
 });
-
-if (mobileNav) {
-    mobileNav.addEventListener("change", () => {
-        changePage(mobileNav.value);
-    });
-}
 
 searchbar.addEventListener("input", displayProjects);
 
